ef0109b4b5eeb136f1f718560ea42bb8b24d8a6a
laura salinas <lara.nirvana67@gmail.com>
2021-07-16 19:25:26 -0400

    changing google api for weatherapi

--- public/script.js
+++ public/script.js
@@ -1,24 +1,18 @@
-const searchElement = document.querySelector('[data-city-search]')
-const searchBox = new google.maps.places.SearchBox(searchElement)
-searchBox.addListener('places_changed', () => {
-  const place = searchBox.getPlaces()[0]
-  if (place == null) return
-  const latitude = place.geometry.location.lat()
-  const longitude = place.geometry.location.lng()
+let btn = document.getElementById('btn-send');
+btn.addEventListener('click', () => {
+    const place = document.getElementById('search').value;
     fetch('/weather', {
-        method: 'GET',
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         },
         body: JSON.stringify({
-            latitude: latitude,
-            longitude: longitude
+            place: place
         })
     })
     .then(response => response.json())
     .then(data => {
         console.log(data)
     })
-    
 })
